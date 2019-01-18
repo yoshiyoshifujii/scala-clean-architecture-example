ef31c40163bf0af729c8aec212b211edf3b29907
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2019-01-18 15:46:23 +0900

    edit

--- modules/entities/src/main/scala/entities/secret/SecretGenerator.scala
+++ modules/entities/src/main/scala/entities/secret/SecretGenerator.scala
@@ -9,8 +9,8 @@ object SecretGenerator {
   private val generator = new PasswordGenerator
 
   private val toCharacterRules: Seq[CharacterRule] = Seq(
-    new CharacterRule(EnglishCharacterData.UpperCase, 4),
-    new CharacterRule(EnglishCharacterData.LowerCase, 4),
+    new CharacterRule(EnglishCharacterData.UpperCase, 2),
+    new CharacterRule(EnglishCharacterData.LowerCase, 2),
     new CharacterRule(EnglishCharacterData.Digit, 1),
     new CharacterRule(
       new org.passay.CharacterData() {
@@ -22,6 +22,6 @@ object SecretGenerator {
     )
   )
 
-  def generate: Secret = Secret(generator.generatePassword(32, toCharacterRules.asJava))
+  def generate: Secret = Secret(generator.generatePassword(8, toCharacterRules.asJava))
 
 }
